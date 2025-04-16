ee31d7d10701ecb434d91a3a27503263e37d41db
husamahmud <husamahmud@gmail.com>
2025-04-17 00:41:58 +0300

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Social Media Avatar Downloader',
+  authors: [{ name: 'Hüsam', url: 'https://www.husam.ninja' }],
+  creator: 'Hüsam',
 }
 
 export default function RootLayout({
